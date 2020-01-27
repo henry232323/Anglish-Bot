6c087dd09b7287adc7901a80edc98d7489ef4cec
henry232323 <henry232323@users.noreply.github.com>
2020-01-26 19:58:15 -0700

    Bot commands

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import asyncio
 import re
 
 import discord
+from discord.ext import commands
 import gspread_asyncio
 from oauth2client.service_account import ServiceAccountCredentials
 
@@ -12,14 +12,64 @@ creds = ServiceAccountCredentials.from_json_keyfile_name('resources/client_secre
 
 # Find a workbook by name and open the first sheet
 # Make sure you use the right name here.
+class Commands(commands.Cog):
+    """Commands for Lookup of Anglish Words"""
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(aliases=["m"])
+    async def match(self, ctx, *, word):
+        """Find entries in the Anglish Wordbook looking for exact matches. Search with Anglish, English, or Old English
+        Example:
+            - /m brook -> brook
+            - /m use -> brook
+            - /m to use -> brook, benoot, upbrook, spurn
+        Ideal for looking up Anglish words to English, or specific English words' translations
+        """
+        cells = await self.bot.sheet.findall(re.compile(rf"^({word})$", re.RegexFlag.IGNORECASE))
+        reduced = {cell.row: cell for cell in cells if cell.row != 1}
+        embeds = []
+        for i, cell in enumerate(reduced.values()):
+            if i == 10:
+                break
+            embed = await self.bot.format_row(ctx, cell, word)
+            embeds.append(embed)
+            await ctx.send(embed=embed)
+
+        if not embeds:
+            await ctx.send("Query not found!")
+
+    @commands.command(aliases=["f"])
+    async def find(self, ctx, *, word):
+        """Find entries in the Anglish Wordbook with soft matches. Search with Anglish, English, or Old English
+        Examples:
+            - /f brook -> upbrook, abrook, brook
+            - /f use -> outler, offcome
+        Better if you aren't quite sure, but looking for more broad answers
+        """
+        rex = re.compile(rf"({word})", re.RegexFlag.IGNORECASE)
+        cells = await self.bot.sheet.findall(rex)
+        reduced = {cell.row: cell for cell in cells if cell.row != 1}
+        embeds = []
+        for i, cell in enumerate(reduced.values()):
+            if i == 10:
+                break
+            embed = await self.bot.format_row(ctx, cell, word)
+            embeds.append(embed)
+            await ctx.send(embed=embed)
+
+        if not embeds:
+            await ctx.send("Query not found!")
 
 
-class Bot(discord.Client):
-    status_message = "//help"
+class Bot(commands.Bot):
+    status_message = "/help for Anglish fun"
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args,
                          game=discord.Game(name=self.status_message),
+                         description="A bot for looking up words in the Anglish wordbook, made by @Henry#8808 (122739797646245899)",
+                         command_prefix="/",
                          **kwargs)
 
         with open("resources/auth") as af:
@@ -29,9 +79,11 @@ class Bot(discord.Client):
         super().run(self._auth)
 
     async def format_row(self, message, cell, query):
+        letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
         headers = ["Word", "Meaning", "Kind", "Forebear", "Whence", "🔨", "Notes"]
         furl = "https://docs.google.com/spreadsheets/d/1y8_11RDvuCRyUK_MXj5K7ZjccgCUDapsPDI5PjaEkMw/edit?gid=0&range={}{}"
-        embed = discord.Embed(color=0xDD0000, title=(await self.sheet.cell(cell.row, 1)).value, url=furl.format(cell.col, cell.row))
+        embed = discord.Embed(color=0xDD0000, title=(await self.sheet.cell(cell.row, 1)).value,
+                              url=furl.format(letters[cell.col], cell.row))
         embed.set_author(name=query, icon_url=message.author.avatar_url)
         for col, h in enumerate(headers):
             c = (await self.sheet.cell(cell.row, col + 1)).value
@@ -43,27 +95,12 @@ class Bot(discord.Client):
         return embed
 
     async def on_ready(self):
+        self.add_cog(Commands(self))
         self.manager = gspread_asyncio.AsyncioGspreadClientManager(lambda *args: creds)
         self.client = await self.manager.authorize()
-        self.workbook = await self.client.open_by_url("https://docs.google.com/spreadsheets/d/1y8_11RDvuCRyUK_MXj5K7ZjccgCUDapsPDI5PjaEkMw/edit?usp=sharing")
+        self.workbook = await self.client.open_by_url(
+            "https://docs.google.com/spreadsheets/d/1y8_11RDvuCRyUK_MXj5K7ZjccgCUDapsPDI5PjaEkMw/edit?usp=sharing")
         self.sheet = await self.workbook.get_worksheet(0)
-
-    async def on_message(self, message):
-        if message.content.startswith("/help"):
-            await message.channel.send("Do /f <word> to look up a word, Anglish, English, or Old English")
-        elif message.content.startswith("/f"):
-            word = message.content.strip("/f ")
-            cells = await self.sheet.findall(re.compile(rf".*({word}).*", re.RegexFlag.IGNORECASE))
-            reduced = {cell.row: cell for cell in cells}
-            embeds = []
-            for i, cell in enumerate(reduced.values()):
-                if i == 10:
-                    break
-                embed = await self.format_row(message, cell, word)
-                embeds.append(embed)
-                await message.channel.send(embed=embed)
-            if not embeds:
-                await message.channel.send("Query not found!")
 
 
 bot = Bot()

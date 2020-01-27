b727f073bb81439d685835d465804c599e227629
Henry <henry232323@users.noreply.github.com>
2020-01-26 17:21:55 -0700

    Update bot

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import asyncio
+import re
 
 import discord
 import gspread_asyncio
 from oauth2client.service_account import ServiceAccountCredentials
 
 # use creds to create a client to interact with the Google Drive API
-scope = ['https://spreadsheets.google.com/feeds']
+scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
 creds = ServiceAccountCredentials.from_json_keyfile_name('resources/client_secret.json', scope)
+
 
 # Find a workbook by name and open the first sheet
 # Make sure you use the right name here.
@@ -26,34 +28,43 @@ class Bot(discord.Client):
     def run(self):
         super().run(self._auth)
 
-    async def format_row(self, cell, query):
-        headers = ["Meaning", "Kind", "Forebear", "Whence", "🔨", "Notes"]
+    async def format_row(self, message, cell, query):
+        headers = ["Word", "Meaning", "Kind", "Forebear", "Whence", "🔨", "Notes"]
         furl = "https://docs.google.com/spreadsheets/d/1y8_11RDvuCRyUK_MXj5K7ZjccgCUDapsPDI5PjaEkMw/edit?gid=0&range={}{}"
-        embed = discord.Embed(color=0xDD0000, title=query, url=furl.format(cell.col, cell.row))
+        embed = discord.Embed(color=0xDD0000, title=(await self.sheet.cell(cell.row, 1)).value, url=furl.format(cell.col, cell.row))
+        embed.set_author(name=query, icon_url=message.author.avatar_url)
         for col, h in enumerate(headers):
-            embed.add_field(name=h, value=(await self.sheet.cell(col, cell.row)).value)
+            c = (await self.sheet.cell(cell.row, col + 1)).value
+            if h == "🔨":
+                c = str(bool(c))
+            if c:
+                embed.add_field(name=h, value=c)
 
         return embed
 
     async def on_ready(self):
         self.manager = gspread_asyncio.AsyncioGspreadClientManager(lambda *args: creds)
         self.client = await self.manager.authorize()
-        self.workbook = await self.client.open("1y8_11RDvuCRyUK_MXj5K7ZjccgCUDapsPDI5PjaEkMw")
+        self.workbook = await self.client.open_by_url("https://docs.google.com/spreadsheets/d/1y8_11RDvuCRyUK_MXj5K7ZjccgCUDapsPDI5PjaEkMw/edit?usp=sharing")
         self.sheet = await self.workbook.get_worksheet(0)
 
     async def on_message(self, message):
         if message.content.startswith("/help"):
-            await message.channel.send("Do /l <word> to look up a word, Anglish, English, or Old English")
-        elif message.content.startswith("/l"):
-            word = message.content.strip("/l ")
-            cells = await self.sheet.findall(word)
+            await message.channel.send("Do /f <word> to look up a word, Anglish, English, or Old English")
+        elif message.content.startswith("/f"):
+            word = message.content.strip("/f ")
+            cells = await self.sheet.findall(re.compile(rf".*({word}).*", re.RegexFlag.IGNORECASE))
+            reduced = {cell.row: cell for cell in cells}
             embeds = []
-            for cell in cells:
-                embed = await self.format_row(cell, word)
+            for i, cell in enumerate(reduced.values()):
+                if i == 10:
+                    break
+                embed = await self.format_row(message, cell, word)
                 embeds.append(embed)
                 await message.channel.send(embed=embed)
             if not embeds:
-                await message.send("Query not found!")
+                await message.channel.send("Query not found!")
+
 
 bot = Bot()
 bot.run()

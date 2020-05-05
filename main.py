7a01cc0df2d5ce10705118ab26da26006be694cd
henry232323 <henrywebsite325@gmail.com>
2020-05-05 15:25:38 -0600

    Add a few new commands for word offerings

--- main.py
+++ main.py
@@ -41,6 +41,31 @@ class Commands(commands.Cog):
         if not embeds:
             await ctx.send("Query not found!")
 
+    async def send_results_mixed(self, ctx, cells, word):
+        reduced = [
+            {cell.row: cell for cell in cells[0] if cell.row != 1},
+            {cell.row: cell for cell in cells[1] if cell.row != 3},
+            {cell.row: cell for cell in cells[2]}
+        ]
+        embeds = []
+        try:
+            async with ctx.typing():
+                async with async_timeout.timeout(300):
+                    for setindex, set in enumerate(reduced):
+                        for i, cell in enumerate(set.values()):
+                            # print(i, cell)
+                            embed = await self.bot.format_row(ctx, cell, word, setindex)
+                            embeds.append(embed)
+
+                            if i == (0 if len(set) == 1 else 1):
+                                paginator = disputils.BotEmbedPaginator(ctx, embeds)
+                                self.bot.loop.create_task(paginator.run())
+        except asyncio.TimeoutError:
+            pass
+
+        if not embeds:
+            await ctx.send("Query not found!")
+
     @commands.command(aliases=["am"])
     async def amatch(self, ctx, *, word):
         """ See the match command, exact same syntax except it searches only the Anglish column """
@@ -99,12 +124,76 @@ class Commands(commands.Cog):
         cells = await self.bot.sheet.findall(rex)
         await self.send_results(ctx, cells, word)
 
+    @commands.command()
+    async def amo(self, ctx, *, word):
+        """ See the amatch command, but also searches the offerings page """
+        rex = re.compile(rf"\b({word})\b", re.RegexFlag.IGNORECASE)
+        cells = [await self.bot.sheet.findall(rex)]
+        cells.append(await self.bot.offersheet1.findall(rex))
+        cells.append(await self.bot.offersheet2.findall(rex))
+        for i in range(len(cells)):
+            cells[i] = (x for x in cells[i] if x.col == 1)
+        await self.send_results_mixed(ctx, cells, word)
+
+    @commands.command()
+    async def ao(self, ctx, *, word):
+        """ See the anglish command, but also searches the offerings page """
+        rex = re.compile(rf"({word})", re.RegexFlag.IGNORECASE)
+        cells = [await self.bot.sheet.findall(rex)]
+        cells.append(await self.bot.offersheet1.findall(rex))
+        cells.append(await self.bot.offersheet2.findall(rex))
+        for i in range(len(cells)):
+            cells[i] = (x for x in cells[i] if x.col == 1)
+        await self.send_results_mixed(ctx, cells, word)
+
+    @commands.command()
+    async def emo(self, ctx, *, word):
+        """ See the ematch command, but also searches the offerings page """
+        rex = re.compile(rf"\b({word})\b", re.RegexFlag.IGNORECASE)
+        cells = [await self.bot.sheet.findall(rex)]
+        cells.append(await self.bot.offersheet1.findall(rex))
+        cells.append(await self.bot.offersheet2.findall(rex))
+        for i in range(len(cells)):
+            cells[i] = (x for x in cells[i] if x.col == 2)
+        await self.send_results_mixed(ctx, cells, word)
+
+    @commands.command()
+    async def eo(self, ctx, *, word):
+        """ See the english command, but also searches the offerings page """
+        rex = re.compile(rf"({word})", re.RegexFlag.IGNORECASE)
+        cells = [await self.bot.sheet.findall(rex)]
+        cells.append(await self.bot.offersheet1.findall(rex))
+        cells.append(await self.bot.offersheet2.findall(rex))
+        for i in range(len(cells)):
+            cells[i] = (x for x in cells[i] if x.col == 2)
+        await self.send_results_mixed(ctx, cells, word)
+
+    @commands.command()
+    async def mo(self, ctx, *, word):
+        """See the match command, but also searches the offerings page """
+        rex = re.compile(rf"\b({word})\b", re.RegexFlag.IGNORECASE)
+        cells = [await self.bot.sheet.findall(rex)]
+        cells.append(await self.bot.offersheet1.findall(rex))
+        cells.append(await self.bot.offersheet2.findall(rex))
+        await self.send_results_mixed(ctx, cells, word)
+
+    @commands.command()
+    async def fo(self, ctx, *, word):
+        """ See the find command, but also searches the offerings page """
+        rex = re.compile(rf"({word})", re.RegexFlag.IGNORECASE)
+        cells = [await self.bot.sheet.findall(rex)]
+        cells.append(await self.bot.offersheet1.findall(rex))
+        cells.append(await self.bot.offersheet2.findall(rex))
+        await self.send_results_mixed(ctx, cells, word)
+
 
 class Bot(commands.Bot):
     manager = None
     client = None
     workbook = None
     sheet = None
+    offersheet1 = None
+    offersheet2 = None
     status_message = "/help for Anglish fun"
     desc = \
         """
@@ -142,6 +231,7 @@ See full descriptions with /help <command>. Good luck y'all, if you have any bug
             self._auth = af.read()
 
         self.loop.create_task(self.workbook_refresh())
+        self.add_cog(Commands(self))
 
     async def workbook_refresh(self):
         while True:
@@ -150,31 +240,61 @@ See full descriptions with /help <command>. Good luck y'all, if you have any bug
             self.workbook = await self.client.open_by_url(
                 "https://docs.google.com/spreadsheets/d/1y8_11RDvuCRyUK_MXj5K7ZjccgCUDapsPDI5PjaEkMw/edit?usp=sharing")
             self.sheet = await self.workbook.get_worksheet(0)
+
+            self.offerworkbook = await self.client.open_by_url(
+                "https://docs.google.com/spreadsheets/d/16f4aeFsNC4oHt3zt-XskiEzcQwU1u-rdh6P8rLCEDOM/edit?usp=sharing")
+            self.offersheet1 = await self.offerworkbook.get_worksheet(0)
+            self.offersheet2 = await self.offerworkbook.get_worksheet(1)
+
+            self.sheets = [self.sheet, self.offersheet1, self.offersheet2]
             await asyncio.sleep(60 * 30)
 
     def run(self):
         super().run(self._auth)
 
-    async def format_row(self, message, cell, query):
-        letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-        headers = ["Word", "Meaning", "Kind", "Forebear", "Whence", "🔨", "Notes"]
-        furl = "https://docs.google.com/spreadsheets/d/1y8_11RDvuCRyUK_MXj5K7ZjccgCUDapsPDI5PjaEkMw/edit?gid=0&range={}{}"
-        embed = discord.Embed(color=0xDD0000, title=(await self.sheet.cell(cell.row, 1)).value,
-                              url=furl.format(letters[cell.col], cell.row))
-        embed.set_author(name=query, icon_url=message.author.avatar_url)
-        for h, c in zip(headers, await self.sheet.row_values(cell.row)):
-            if h == "🔨":
-                c = str(bool(c))
-            if c:
-                embed.add_field(name=h, value=c)
+    async def format_row(self, message, cell, query, setindex=None):
+        if setindex is None:
+            letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+            headers = ["Word", "Meaning", "Kind", "Forebear", "Whence", "🔨", "Notes"]
+            furl = "https://docs.google.com/spreadsheets/d/1y8_11RDvuCRyUK_MXj5K7ZjccgCUDapsPDI5PjaEkMw/edit?gid=0&range={}{}"
+            embed = discord.Embed(color=0xDD0000, title=(await self.sheet.cell(cell.row, 1)).value,
+                                  url=furl.format(letters[cell.col], cell.row))
+            embed.set_author(name=query, icon_url=message.author.avatar_url)
+            for h, c in zip(headers, await self.sheet.row_values(cell.row)):
+                if h == "🔨":
+                    c = str(bool(c))
+                if c:
+                    embed.add_field(name=h, value=c)
 
-        embed.add_field(name="Help",
-            value="Use /help for command usage. If the bot is typing it is still generating new results that the page number might not reflect")
+            embed.add_field(name="Help",
+                            value="Use /help for command usage. If the bot is typing it is still generating new results that the page number might not reflect")
 
-        return embed
+            return embed
+        else:
+            letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+            headers = ["Word", "Meaning", "Kind", "Forebear", "Whence", "🔨", "Notes", "Who?", "Source"]
+            furl = [
+                "https://docs.google.com/spreadsheets/d/1y8_11RDvuCRyUK_MXj5K7ZjccgCUDapsPDI5PjaEkMw/edit?gid=0&range={}{}",
+                "https://docs.google.com/spreadsheets/d/16f4aeFsNC4oHt3zt-XskiEzcQwU1u-rdh6P8rLCEDOM/edit#gid=1186039826&range={}{}",
+                "https://docs.google.com/spreadsheets/d/16f4aeFsNC4oHt3zt-XskiEzcQwU1u-rdh6P8rLCEDOM/edit#gid=1193230534&range={}{}",
+            ]
+            embed = discord.Embed(color=0xDD0000, title=(await self.sheets[setindex].cell(cell.row, 1)).value,
+                                  url=furl[setindex].format(letters[cell.col], cell.row))
+            embed.set_author(name=query, icon_url=message.author.avatar_url)
+            statuses = ["In Wordbook", "Offered", "Rejected"]
 
-    async def on_ready(self):
-        self.add_cog(Commands(self))
+            for h, c in zip(headers, await self.sheets[setindex].row_values(cell.row)):
+                if h == "🔨":
+                    c = str(bool(c))
+                if c:
+                    embed.add_field(name=h, value=c)
+                    
+            embed.add_field(name="Status", value=statuses[setindex])
+
+            embed.add_field(name="Help",
+                            value="Use /help for command usage. If the bot is typing it is still generating new results that the page number might not reflect")
+
+            return embed
 
 
 bot = Bot()
